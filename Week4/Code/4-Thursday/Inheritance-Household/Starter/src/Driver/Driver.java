66fc46017769b91b63dec8ae2d6007bd64bdc360
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-08-14 15:25:43 -0400

    Inheritance household lab added

---
+++ Week4/Code/4-Thursday/Inheritance-Household/Starter/src/Driver/Driver.java
@@ -0,0 +1,9 @@
+package Driver;
+
+public class Driver {
+    public static void main(String[] args) {
+        //Create households and add to list or array
+
+        //iterate through list or array and perform required actions
+    }
+}
