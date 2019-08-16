d2504b85607e7e193056294f871fa67a2b7d41cb
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-08-16 14:47:45 -0400

    FavoriteThings demo added

---
+++ Week4/Code/2-Tuesday/FavoriteThings/src/FavoriteThings.java
@@ -0,0 +1,12 @@
+class FavoriteThings {
+    public static void main(String[] args) {
+        // Shortened syntax: Assign variable, allocate memory, and initialize values all in one statement.
+        String[] favoriteThings = {
+                "raindrops",
+                "roses",
+                "whiskers on kittens"
+        }; // Length will be inferred as 3.
+
+        System.out.println(favoriteThings.length);
+    }
+}
