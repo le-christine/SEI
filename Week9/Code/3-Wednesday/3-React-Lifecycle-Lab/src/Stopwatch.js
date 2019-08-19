b5d43ffdf4f4923d0fa7cbd74f31202c0c62701c
Casey Harding <harding@brooklynfoundry.com>
2019-08-19 14:39:33 -0400

    [3-Wednesday] React stopwatch lab bonus questions complete.

--- Week9/Code/3-Wednesday/3-React-Lifecycle-Lab/src/Stopwatch.js
+++ Week9/Code/3-Wednesday/3-React-Lifecycle-Lab/src/Stopwatch.js
@@ -6,50 +6,84 @@ class Stopwatch extends Component {
         super(props);
 
         this.state = {
-            counter: 0,
-            increment: false
+            counters: [{
+                counter: 0,
+                increment: false,
+                started: false,
+            }]
         }
 
         this.handleStart = this.handleStart.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.handlePause = this.handlePause.bind(this);
+        this.addCounter = this.addCounter.bind(this);
     }
 
-    handleStart() {
-        if (!this.state.increment) {
-            this.incrementer = setInterval( () =>
-              this.setState({
-                counter: this.state.counter + 1,
-                increment: true
-              })
-            , 1000);
-        }
-    }
-
-    handleReset() {
-        clearInterval(this.incrementer);
+    addCounter() {
         this.setState({
-            counter: 0,
-            increment: false
+            counters: [...this.state.counters, {
+                counter: 0,
+                increment: false,
+                started: false,
+            }]
         })
     }
 
-    handlePause() {
-        clearInterval(this.incrementer);
+    handleStart(index) {
+        console.log(this.state.counters);
+        if (!this.state.increment) {
+            let counters = this.state.counters;
+            counters[index].incrementer = setInterval( () => {
+                let counters = this.state.counters;
+                counters[index].counter++;
+                counters[index].increment = true;
+                counters[index].started = true;
+                this.setState({
+                    counters: counters,
+                })}, 1000
+            );
+        }
+    }
+
+    handleReset(index) {
+        let counters = this.state.counters;
+        clearInterval(counters[index].incrementer);
+        counters[index].counter = 0;
+        counters[index].increment = false;
+        counters[index].started = false;
+
         this.setState({
-            increment: false
+            counters: counters,
+        })
+    }
+
+    handlePause(index) {
+        let counters = this.state.counters;
+        clearInterval(counters[index].incrementer);
+        counters[index].increment = false;
+        counters[index].started = false;
+
+        this.setState({
+            counters: counters
         })
     }
 
     render() {
         return (
             <div className="stopwatch">
-            <h1>{this.state.counter}</h1>
-                <div className="controls">
-                    <button onClick={this.handleReset}>Reset</button>
-                    <button onClick={this.handleStart}>Start</button>
-                    <button onClick={this.handlePause}>Pause</button>
-                </div>
+                <button onClick={this.addCounter}>Add a counter!</button>
+                { this.state.counters.map((counter, index) => {
+                    return (
+                        <div key={index}>
+                            <h1>{counter.counter}</h1>
+                            <div className="controls">
+                                <button onClick={() => this.handleStart(index)}>Start</button>
+                                { (counter.started || counter.counter > 0) && <button onClick={() => this.handlePause(index)}>Pause</button> }
+                                { (counter.started || counter.counter > 0) && <button onClick={() => this.handleReset(index)}>Reset</button> }
+                            </div>
+                        </div>
+                    )}
+                )}
             </div>
         );
     }
