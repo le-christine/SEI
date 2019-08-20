92d2a8239b9f64c454362d42debeb4dc084d264d
Casey Harding <harding@brooklynfoundry.com>
2019-08-20 10:41:53 -0400

    [4-Thursday] [2-Styling-Components-In-React-Solution-Code] Solution code in place.

---
+++ Week9/Code/4-Thursday/2-Styling-Components-In-React-Solution-Code/insta-clone/src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { FaInstagram, FaUser, FaHeart, FaCompass, FaSearch } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 26px 5vw;
+  width: 90vw;
+  height: 25px;
+  border-bottom: 1px solid rgba(0,0,0,.0975);
+  p{
+    font-size: 1.75rem;
+    font-family: 'Oleo Script', cursive;
+    margin: 0;
+    svg{
+      margin-bottom: 5px;
+    }
+  }
+`
+
+const SearchContainer = styled.div`
+  background: #fafafa;
+  border: 1px solid #dbdbdb;
+  width: 25%;
+  padding: 7px;
+  border-radius: 3px;
+  color: #999;
+  svg{
+    margin-bottom: 5px;
+  }
+  input{
+    border: none;
+    font-weight: 300;
+    background: transparent;
+    text-align: center;
+  }
+`;
+
+const IconsContainer = styled.div`
+  width: 25%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+class Navbar extends Component{
+  render(){
+    return (
+      <Nav>
+        <p><FaInstagram /> | Instaclone</p>
+        <SearchContainer>
+          <FaSearch/><input type="text" placeholder="Search"/>
+        </SearchContainer>
+        <IconsContainer>
+          <FaCompass size={28} />
+          <FaHeart size={28} />
+          <FaUser size={28} />
+        </IconsContainer>
+      </Nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ Week9/Code/4-Thursday/2-Styling-Components-In-React-Solution-Code/insta-clone/src/components/Post.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import Comment from './Comment'
+import styled from 'styled-components'
+import { FaHeart, FaComment } from 'react-icons/fa'
+
+const PostContainer = styled.div`
+  background-color: #fff;
+  border-radius: 3px;
+  width: 95vw;
+  max-width: 600px;
+  margin: 20px auto;
+  border: 1px solid #e6e6e6;
+`
+
+const PostUser = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 16px;
+  img{
+    height: 30px;
+    width: 30px;
+    border-radius: 100%;
+  }
+  span{
+    padding-left: 5px;
+    font-weight: bold;
+  }
+`
+
+const PostContent = styled.div`
+  img{
+    max-width: 600px;
+    margin: 0 auto;
+  }
+`
+
+const PostInfo = styled.div`
+  padding: 5px 16px;
+`
+
+const PostActions = styled.div`
+  svg{
+    padding: 5px;
+  }
+`
+
+class Post extends Component {
+
+  render () {
+    const { post } = this.props
+
+    const likes = post.likes.reduce((prev, curr, index) => {
+      if (index === 0) {
+        return `${curr.username}`
+      } else if (index === post.likes.length - 1) {
+        return `${prev} and ${curr.username}`
+      } else {
+        return `${prev}, ${curr.username}`
+      }
+    }, '')
+    return (
+      <PostContainer>
+        <PostUser>
+          <img src={post.user.profile_pic} alt={post.user.username} />
+          <span>{post.user.username}</span>
+        </PostUser>
+        <PostContent>
+          <img src={post.image.url} />
+        </PostContent>
+        <PostInfo>
+          <p>{post.caption}</p>
+          <PostActions>
+            <FaHeart size={35} />
+            <FaComment size={35} />
+          </PostActions>
+          <p><strong>{likes}</strong> like this</p>
+          {post.comments.map((comment, i) => <Comment key={i} comment={comment}/>)}
+        </PostInfo>
+      </PostContainer>
+    )
+  }
+}
+
+export default Post
