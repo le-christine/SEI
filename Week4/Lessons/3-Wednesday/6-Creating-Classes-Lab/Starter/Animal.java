840793bf9acdd445e4634d0b3fddfe8686f321e4
lorem <lorem@ipsum.com>
2019-09-18 10:43:00 -0400

    [Week 4] [3-Wednesday] [Creating Classes Lab] Updated starter code.

---
+++ Week4/Lessons/3-Wednesday/6-Creating-Classes-Lab/Starter/Animal.java
@@ -0,0 +1,38 @@
+public class Animal {
+    private int numLegs;
+    private int topSpeed;
+    private boolean isEndangered;
+    private String name;
+
+    public Animal(int numLegs, int topSpeed, boolean isEndangered, String name) {
+        //Set properties here
+    }
+
+    //Put getters and setters here
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        //Instantiate new Animal
+        Animal animal = ;
+
+        String name;
+        int topSpeed;
+
+        //get name and speed values using getters
+        name = ;
+        topSpeed = ;
+
+        //Print some output
+        System.out.println();
+
+        //Set new name, speed, and endangered properties values using setters
+
+        //get new values using getters
+        name = ;
+        topSpeed = ;
+
+        //Print some output
+        System.out.println();
+    }
+}
