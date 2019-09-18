266b9e6addd7d5d1e4e7fd2d05f8cbc5f0e7e6ea
lorem <lorem@ipsum.com>
2019-09-18 10:26:53 -0400

    [Week 4] [3-Wednesday] [Creating Classes Lab] Added starter code.

---
+++ Week4/Lessons/3-Wednesday/6-Creating-Classes-Lab/Starter/src/Main.java
@@ -0,0 +1,32 @@
+import ly.generalassemb.suresh.classes_lab.Animal;
+
+// import Animal class
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        //Instantiate new Animal
+//        Animal animal = ;
+
+        String name;
+        int topSpeed;
+
+        //get name and speed values using getters
+//        name = ;
+//        topSpeed = ;
+
+        //Print some output
+//        output.setText();
+
+        //Set new name, speed, and endangered properties values using setters
+
+
+        //get new values using getters
+//        name = ;
+//        topSpeed = ;
+
+        //Print some output
+//        changedOutput.setText();
+    }
+}
