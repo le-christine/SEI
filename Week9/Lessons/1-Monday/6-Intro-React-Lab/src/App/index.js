3c80a475875cb32e9957113529b8512231e70a98
Christine Le <christine.le244@gmail.com>
2019-10-21 19:08:23 -0400

    Able to loop through each recipe and make components from the contents

--- Week9/Lessons/1-Monday/6-Intro-React-Lab/src/App/index.js
+++ Week9/Lessons/1-Monday/6-Intro-React-Lab/src/App/index.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import './style.css';
-import RecipeHeader from './RecipeHeader.js'
-import Nav from './nav.js'
-import Article from './Article.js'
-import Footer from './Footer.js'
-import RecipePrep from './RecipePrep.js'
+import RecipeHeader from './RecipeHeader.js';
+import Nav from './nav.js';
+import Article from './Article.js';
+import Footer from './Footer.js';
+import RecipePrep from './RecipePrep.js';
 
 import recipes from '../data/recipes';
 
-const recipe = recipes[0];
-const recipe1 = recipes[1];
-
 class App extends Component {
   render() {
-
+    let recipeList = recipes.map((data) =>
+    <div>
+    <RecipeHeader recipe={data}/>
+    <RecipePrep recipe={data}/>
+    <Article recipe={data}/>
+    </div>
+  )
 
     return (
       <div className="flex-container">
         <Nav/>
-
-        <RecipeHeader recipe = {recipe}/>
-        <RecipePrep recipe = {recipe}/>
-        <Article recipe={recipe}/>
-
-        <RecipeHeader recipe = {recipe1}/>
-        <RecipePrep recipe = {recipe1}/>
-        <Article recipe={recipe1}/>
-
+        {recipeList}
         <Footer/>
       </div>
     );
