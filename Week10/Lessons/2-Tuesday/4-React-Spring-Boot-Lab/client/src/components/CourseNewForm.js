a3aa63d961309e3face7c92c6651379363d2aeab
Christine Le <christine.le244@gmail.com>
2019-10-29 15:24:53 -0400

    [src] [components] [CourseNewForm] created a component to add a new course

--- Week10/Lessons/2-Tuesday/4-React-Spring-Boot-Lab/client/src/components/CourseNewForm.js
+++ Week10/Lessons/2-Tuesday/4-React-Spring-Boot-Lab/client/src/components/CourseNewForm.js
@@ -1,5 +1,57 @@
 import React, { Component } from 'react';
 
 class CourseNewForm extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newCourse: {}
+    }
+  }
+
+  handleChange = (e) => {
+    const attributeToChange = e.target.name;
+    const newValue = e.target.value;
+
+    const updatedNewCourse = {...this.state.newCourse};
+    updatedNewCourse[attributeToChange] = newValue;
+    this.setState({newCourse:updatedNewCourse});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Create a new course</h2>
+
+        <form>
+          <div>
+            <label htmlFor="title">Name</label>
+            <input
+              name="name"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="description">Code</label>
+            <input
+              name="code"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <input type="submit" value="Create Course"/>
+          </div>
+        </form>
+        <hr/>
+        <hr/>
+      </div>
+    )
+  }
+
 }
+
+export default CourseNewForm;
